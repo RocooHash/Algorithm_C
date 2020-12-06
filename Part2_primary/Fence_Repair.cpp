a24b32fb4fc6098fce1385a55dede75b00622ff5
RocooHash <1154035918@qq.com>
2020-12-06 21:26:01 +0800

    12-6

--- Part2_primary/Fence_Repair.cpp
+++ Part2_primary/Fence_Repair.cpp
@@ -1,10 +1,53 @@
 /*POJ:3253*/
 #include <iostream>
-#include <vector>
 #include <algorithm>
 using namespace std;
+typedef long long ll;
+
+int N, L[20001];
+
+void solve()
+{
+    ll ans = 0;
+    while (N > 1)
+    {
+        int min1 = 0, min2 = 1;
+        if (L[min1] > L[min2])
+            swap(L[min1], L[min2]);
+
+        for (int i = 2; i < N; i++)
+        {
+            if (L[i] < L[min1])
+            {
+                min2 = min1;
+                min1 = i;
+            }
+            else if (L[i] < L[min2])
+            {
+                min2 = i;
+            }
+        }
+
+        int t = L[min1] + L[min2];
+        ans += t;
+
+        if (min1 == N - 1)
+            swap(min1, min2);
+        L[min1] = t;
+        L[min2] = L[N - 1];
+        N--;
+    }
+
+    cout << ans;
+}
 
 int main()
 {
-    int 
+    cin >> N;
+    for (int i = 0; i < N; i++)
+    {
+        cin >> L[i];
+    }
+    solve();
+    return 1;
 }
